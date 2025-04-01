46f7c33c4eaa48602c089c1e3e296af6631f4d7d
tonichl <158068539+tonichl@users.noreply.github.com>
2025-04-01 19:36:37 +0000

    start lab 11

---
+++ lab11_chen/first_app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>toni chen liang</h1>
+      <p>this is my first reactJS app</p>
+
+    </div>
+  );
+}
+
+export default App;
